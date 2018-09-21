5405979b6145fdfcd05457620908c5f60424a1b1
user-zhangyanan <1546079149@qq.com>
2018-09-21 14:10:53 +0800

    first commit

---
+++ app/src/main/java/com/bawei/dome_rcycleviewstyle/SwipeCardBean.java
@@ -0,0 +1,16 @@
+package com.bawei.dome_rcycleviewstyle;
+
+/**
+ * Created by hasee on 2018/9/21.
+ */
+
+public class SwipeCardBean {
+
+    public String title;
+    public int resoutimage;
+
+
+
+
+
+}
